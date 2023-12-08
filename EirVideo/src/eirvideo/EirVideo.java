52b30dd4e5756b4dc4b8317f3f9b838ad5f9ca2e
Jair130 <2021231@student.cct.ie>
2023-12-08 13:03:49 +0000

    first attempt to user interface

--- EirVideo/src/eirvideo/EirVideo.java
+++ EirVideo/src/eirvideo/EirVideo.java
@@ -8,12 +8,41 @@ package eirvideo;
 import  eirvideo.Products.Product;
 import eirvideo.Catalogs.Catalog;
 import eirvideo.Products.Movie;
+import eirvideo.Users.Users;
+import eirvideo.Users.UsersManager;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class EirVideo {
 
   
     public static void main(String[] args) {
+        
+        
+        Users user1 = new Users ("jair","jair13");
+        
+        Users user2 = new Users ("max","max123");
+        
+        UsersManager.addUser(user1);
+        
+        UsersManager.addUser(user2);
+        
+       // Log in 
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("TYPE USERNAME:");
+        String Username = scanner.nextLine();
+        
+        System.out.println("TYPE PASSWORD:");
+        String password = scanner.nextLine();
+        
+        if (UsersManager.validateCredentials(Username, password)) {
+            System.out.println("WELCOME TO EIRVIDEO!!, THIS ARE THE MOVIE AVAILABLES:");
+            // Add an else case
+        } else {
+            System.out.println("USERNAME OR PASSWORD INCORRECT, TRY AGAIN!");
+        }
+        
          Product movie1 = new Movie("PulpFiction", 3.99);
          Product movie2 = new Movie("Napoleon", 7.99);
          
