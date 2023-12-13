fc084f34e3c7a3e20755e246877a95054f427e63
Jair130 <2021231@student.cct.ie>
2023-12-13 12:52:17 +0000

    user log in class

---
+++ EirVideo/src/Users/UserLogIn.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Users;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author djap
+ */
+
+public class UserLogIn {
+    
+    private ArrayList<Users> users;
+
+    public UserLogIn(ArrayList<Users> users) {
+        this.users = users;
+    }
+
+    public UserLogIn() {
+        
+    }
+
+    
+
+    public boolean UserLogIn(String Username, String Password) {
+        for (Users user : users) {
+            if (user.getUsername().equals(Username)
+                    && user.getEncryptedPassword().equals(user.encryptPassword(Password))) {
+                System.out.println("LOG IN SUCCESSFUL, WELCOME TO EIRVIDEO!");
+                return true;
+            }
+        }
+        System.out.println("USERNAME OR PASSWORD INCORRECT");
+        return false;
+    }
+
+    public ArrayList<Users> getUsers() {
+        return users;
+    }
+    
+}
