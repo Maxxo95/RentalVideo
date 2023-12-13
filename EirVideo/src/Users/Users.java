dcab41d980b6df26479ce53116788996d64fbec5
Jair130 <2021231@student.cct.ie>
2023-12-13 12:52:39 +0000

    users third attempt

--- EirVideo/src/Users/Users.java
+++ EirVideo/src/Users/Users.java
@@ -7,16 +7,21 @@ package Users;
 /**
  *
  * @author djap 2021231 */
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+
+
 public class Users {
     
-    public String Username;
-    public String Password;
+    private  String Username;
+    private  String EncryptedPassword;
     
     //Constructor,getter and setters
     
     public Users(String Username, String Password) {
         this.Username = Username;
-        this.Password = Password;
+        this.EncryptedPassword = encryptPassword(Password);
         
     }
 
@@ -24,16 +29,32 @@ public class Users {
         return Username;
     }
 
-    public String getPassword() {
-        return Password;
+    public String getEncryptedPassword() {
+        return EncryptedPassword;
     }
     
-    
-    
-    
-    
-    
-    
-    
-    
+    public String encryptPassword(String Password) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] bytes = md.digest(Password.getBytes());
+            StringBuilder sb = new StringBuilder();
+
+            for (byte aByte : bytes) {
+                sb.append(Integer.toString((aByte & 0xff) + 0x100, 16).substring(1));
+            }
+
+            return sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            return null;
+        }
+    }
 }
+    
+    
+    
+    
+    
+    
+    
+    
+
